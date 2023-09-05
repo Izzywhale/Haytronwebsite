f90b34bcb4006b9db3ff208349e19ce9f4927576
IzzyWhale <osobajoisrael@gmail.com>
2023-09-05 21:01:25 +0100

    progress on haytron landing page

---
+++ index.js
@@ -0,0 +1,6 @@
+const swiper = new Swiper(...)
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
